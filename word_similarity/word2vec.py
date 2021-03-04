b71dfe7e43c980e85fdb8e07ea6ff2f9076e93bd
Meng <75422140+mengzhu0308@users.noreply.github.com>
2021-03-05 06:55:40 +0800

    Update word2vec.py

--- word_similarity/word2vec.py
+++ word_similarity/word2vec.py
@@ -11,10 +11,11 @@ from keras.layers import *
 
 def cbow(x, vocub_size, word_dim, input_length):
     x = Embedding(vocub_size, word_dim, input_length=input_length)(x)
-    x = Lambda(lambda arg: K.mean(arg, axis=1))(x)
+    x = Lambda(lambda arg: K.sum(arg, axis=1))(x)
     x = Dense(vocub_size, activation='softmax')(x)
     return x
 
 def skip_gram(x, vocub_size, word_dim, window_size):
     x = Embedding(vocub_size, word_dim, input_length=1)(x)
+    x = Lambda(lambda arg: arg[:, 0, :])(x)
     return [Dense(vocub_size, activation='softmax')(x) for _ in range(window_size * 2)]
